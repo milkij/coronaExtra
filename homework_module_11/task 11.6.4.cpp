5b655597d141b1432200c4ac05e2920d11b1bde0
alexandershirokov <Natasha3637242>
2021-08-20 09:07:59 +0300

    change numb om tasks

--- homework_module_11/task 11.6.4.cpp
+++ homework_module_11/task 11.6.4.cpp
@@ -43,9 +43,25 @@ bool checkFloat (std::string someFloatNumber) {
 int sumOfChars(std::string x) {
     int sumOf = 0;
     for (int i = 0; i < x.length(); i++) {
-        if(x[i]!='.') sumOf+=(int) x[i];
+        if(x[i]=='.') sumOf+=0;
+        else sumOf+=(int)x[i];
+
     }
     return sumOf;
+}
+
+std::string cutZeros(std::string s) {
+    int i=0,j=s.length();
+    for (;i<s.length();) {
+        if(s[i]=='0') i++;
+        else break;
+    }
+    for (;j!=0;j--) {
+        if(s[j]=='0') continue;
+        else break;
+    }
+    if (s.find('.')!=-1) return s.substr(i,j-1);
+    else return s.substr(i,j);
 }
 
 int main () {
@@ -55,13 +71,16 @@ int main () {
     std::string y;
     std::cin >> y;
 
+    x = cutZeros(x);
+    y = cutZeros(y);
+    std::cout << x <<std::endl;
+    std::cout << y <<std::endl;
     if (checkFloat(x) && checkFloat(y)) {
         int first = sumOfChars(x), second = sumOfChars(y);
         if(first<second) std::cout << "less" << std::endl;
         else if (first>second) std::cout << "more" << std::endl;
         else std::cout << "equal" << std::endl;
     } else std::cout << "Error" << std::endl;
-
 
 
 }
