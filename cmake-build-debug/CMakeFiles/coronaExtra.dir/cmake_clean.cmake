858fd45000915f1128b26c650bd96f773653ef0b
alexandershirokov <Natasha3637242>
2021-09-09 18:09:46 +0300

    Add task 12.4.3.

--- cmake-build-debug/CMakeFiles/coronaExtra.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/coronaExtra.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/coronaExtra.dir/homework_module_12/task_12.4.2.cpp.o"
+  "CMakeFiles/coronaExtra.dir/homework_module_12/task_12.4.3.cpp.o"
   "coronaExtra"
   "coronaExtra.pdb"
 )
---
+++ homework_module_12/task 12.4.3.cpp
@@ -0,0 +1,23 @@
+//
+// Created by  Milkij on 09.09.2021.
+//
+#include "iostream"
+
+int main(){
+    float floatNumbers[15];
+    std::cout << "Input 15 flaot naumbers" << std::endl;
+    for (int i=0; i<15; i++){
+        std::cin >> floatNumbers[i];
+    }
+    float x;
+    for (int i=0;i<15;i++){
+        for (int j=0;j<14;j++){
+            if(floatNumbers[j]>floatNumbers[j+1]){
+               x=floatNumbers[j];
+               floatNumbers[j]=floatNumbers[j+1];
+               floatNumbers[j+1]=x;
+            }
+        }
+    }
+    for(int i=0;i<15;i++) std::cout << floatNumbers[i] << " ";
+}
