6c61aea33c9037b6d941aefec4aec7712efbc6d6
shirokov.mos@ya.ru <shirokov.mos@ya.ru>
2021-11-24 17:43:39 +0300

    add up-to 13.4.2

--- module_13/homework_13_4/task 13.4.2.cpp
+++ module_13/homework_13_4/task 13.4.2.cpp
@@ -1,4 +1,19 @@
 //
 // Created by Александр Широков on 24.11.2021.
 //
+#include "iostream"
+#include "vector"
 
+int main(){
+    int N;
+    std::vector<int> robots;
+    while (N!=-1) {
+        std::cout<< "Input robot number: ";
+        std::cin >> N;
+        while(robots.size() != 0 && N != -1 && robots[robots.size()-1] < N) robots.pop_back();
+        robots.push_back(N);
+        //std::cout << robots.capacity() << " ";
+    }
+    for (int i=0; i<robots.size(); i++) std::cout<< robots[i] << " ";
+
+}
