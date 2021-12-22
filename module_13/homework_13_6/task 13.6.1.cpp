1e7d9c6b80e73ab4359755902567b1ade67a7f4d
shirokov.mos@ya.ru <shirokov.mos@ya.ru>
2021-12-22 17:10:47 +0300

    13.6.3 has been fixed

--- module_13/homework_13_6/task 13.6.1.cpp
+++ module_13/homework_13_6/task 13.6.1.cpp
@@ -6,19 +6,32 @@
 
 std::vector<int> newVec(std::vector<int> vec, int number) {
     if (vec.size()==0 || number==-1) return vec;
-    int count=0;
-    for (int i=0; i<vec.size(); i++) if(vec[i]==number) count++;
-    std::vector<int> newVec;
-    for (int i=0; i<vec.size();i++) if(vec[i]!=number) newVec.push_back(vec[i]);
-    if (count==0) newVec.push_back(number);
+    //int count=0;
+    //for (int i=0; i<vec.size(); i++) if(vec[i]==number) count++;
+    //std::vector<int> newVec;
+    //for (int i=0; i<vec.size();i++) if(vec[i]!=number) newVec.push_back(vec[i]);
+    //if (count==0) newVec.push_back(number);
+    //return newVec;
+    using namespace std;
+    vector<int> newVec(vec.size());
+    int j=0;
+    for (int i=0; i<vec.size(); i++) {
+        if(vec[i]!=number) {
+            newVec[j]=vec[i];
+            ++j;
+        }
+    }
+    newVec.resize(j);
+    if (newVec.size()==vec.size()) newVec.push_back(number);
     return newVec;
+
 }
 
 int main(){
     std::vector<int> vec = {1, 3, 3, 5, 1};
     int n;
-    std::cout << "input a number: ";
     while (n!=-1){
+        std::cout << "input a number: ";
         std::cin >> n;
         vec=newVec(vec,n);
 
